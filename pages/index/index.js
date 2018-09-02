303dd6763a1c862a07f98ad12e5f53d541eb7cf6
Shinelon <867070117@qq.com>
2018-09-02 10:18:34 +0800

    增加函数

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,10 +4,7 @@
 
  Page({
    data: {
-     motto: 'Hello World',
-     userInfo: {},
-     hasUserInfo: false,
-     canIUse: wx.canIUse('button.open-type.getUserInfo')
+     
    },
    //事件处理函数
    bindViewTap: function() {
@@ -16,39 +13,75 @@
      })
    },
    onLoad: function () {
-     if (app.globalData.userInfo) {
-       this.setData({
-         userInfo: app.globalData.userInfo,
-         hasUserInfo: true
-       })
-     } else if (this.data.canIUse){
-       // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-       // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-         this.setData({
-           userInfo: res.userInfo,
-           hasUserInfo: true
-         })
-       }
-     } else {
-       // 在没有 open-type=getUserInfo 版本的兼容处理
-       wx.getUserInfo({
-         success: res => {
-           app.globalData.userInfo = res.userInfo
-           this.setData({
-             userInfo: res.userInfo,
-             hasUserInfo: true
-           })
-         }
-       })
-     }
+     
    },
-   getUserInfo: function(e) {
-     console.log(e)
-     app.globalData.userInfo = e.detail.userInfo
-     this.setData({
-       userInfo: e.detail.userInfo,
-       hasUserInfo: true
-     })
+   getAuthorMessage: function(e) {
+     wx.getSetting({
+       success: function (res) {
+         if (res.authSetting['scope.userInfo']) {
+           // 已经授权，可以直接调用 getUserInfo 获取头像昵称
+           wx.getUserInfo({
+             success: function (res) {
+               //进行视频链接操作
+               wx.request({
+                 url: 'https://www.layzz.cn/content/video', //仅为示例，并非真实的接口地址
+                 data: {
+                   openId: '45678',
+                   link: 'http://v.douyin.com/dFmuq6/'
+                 },
+                 header: {
+                   'content-type': 'application/json' // 默认值
+                 },
+                 success: function (res) {
+                  var video = res.data.data.video;
+                   if (res.data.retCode === 400) {
+                     wx.showToast({
+                       title: '视频地址有误',
+                       icon: 'none'
+                     })
+                   } else if (res.data.retCode === 200) {
+                     //获取用户相册权限
+                     wx.getSetting({
+                       success: function (res) {
+                         if (res.authSetting['scope.writePhotosAlbum']) {
+                          console.log("走到我这里")
+                           wx.saveVideoToPhotosAlbum({
+                             filePath: video,
+                             success(res) {
+                               console.log("成功了哟")
+                             },
+                             fail(res) {
+                               console.log("失败了哟")
+                             }
+                           }
+                           )
+                         }else {
+                           wx.saveVideoToPhotosAlbum({
+                             filePath: video,
+                             success(res) {
+                               console.log("成功了哟")
+                               console.log(res.errMsg)
+                             },
+                             fail(res) {
+                               console.log("失败了哟")
+                               console.log(res.errMsg)
+                             }
+                           }
+                           )
+                         }
+                       }
+                     })
+
+
+                   }
+                 }
+               })
+             }
+           })
+              
+          }
+       }
+      })
    }
- })
+  })
+ 
