7c8c205cce62d9db764b91f8f32aaff2b5a12a66
Shinelon <867070117@qq.com>
2018-09-02 17:59:32 +0800

    增加url处理

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,7 +4,8 @@
 
  Page({
    data: {
-     'userUrl':''
+     'userUrl':'',
+     link:''
    },
    //获取用户输入的用户名
    userUrlInput: function (e) {
@@ -24,38 +25,45 @@
    getAuthorMessage: function(e) {
      var that = this;
      var btnUrl = this.data.userUrl;
-     console.log(btnUrl)
      wx.getSetting({ 
        success: function (res) {
          if (res.authSetting['scope.userInfo']) {
+           wx.showLoading({
+             title: '加载中',
+           }),
            // 已经授权，可以直接调用 getUserInfo 获取头像昵称
            wx.getUserInfo({
              success: function (res) {
                //进行视频链接操作
                wx.request({
-                 url: 'https://www.layzz.cn/content/video', //仅为示例，并非真实的接口地址
+                 url: 'http://localhost:9093/video', //仅为示例，并非真实的接口地址
                  data: {
-                   openId: '45678',
-                   link: btnUrl
+                   openId: "4567854",
+                   link:btnUrl+""
                  },
                  header: {
                    'content-type': 'application/json' // 默认值
                  },
                  success: function (res) {
                    var video = res.data.data;
-                   console.log(res.data)
+                   console.log(video+"====video")
+                   console.log(btnUrl + "====btnUerl")
                    if (res.data.retCode === '0002') {
                      wx.showToast({
                        title: '视频地址有误',
                        icon: 'none'
                      })
+                     wx.hideLoading()
                    } else if (res.data.code === '0001') {
-                  
-                    //  this.userUrl = video; //无效不能实时的渲染到页面
-                    //  that.setData({ userUrl: video });//和页面进行绑定可以动态的渲染到页面
-                    //  //将地址返回给前端
+                      this.userUrl = video; //无效不能实时的渲染到页面
+                      that.setData({ userUrl: video });//和页面进行绑定可以动态的渲染到页面
+                      that.setData({link:video});
+                      wx.hideLoading()
 
                    }
+                 },
+                 fail:function(res) {
+                   wx.hideLoading();
                  }
                }
                )
